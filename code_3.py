fb43d06ee950357e37f478fd1f41cc65ea9f252f
Ada <139880994+Adamay01@users.noreply.github.com>
2023-11-12 12:36:35 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,71 @@
+    });
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+}
+main();
+
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    const data = generateRandomData();
+function main() {
+
+    }
+    return data;
+function generateRandomData() {
+if __name__ == "__main__":
+    main()
+    return data
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+    data = generate_random_data()
+
+
+import random
+
+    for item in data:
+def main():    }
+main();
+function generateRandomData() {
+
+    data.forEach(item => {
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+}
+    });
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+}
+    const data = generateRandomData();
+    return data;
+
+        console.log("Random Number: " + item);
+import random
+    main()
+
+if __name__ == "__main__":
+def main():
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    data = generate_random_data()
+    return data
+    for item in data:
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+if __name__ == "__main__":
+import random
+    return data
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+
+    main()
